1796bcf30a5a80b5e7249ded0388cc56ea01f19d
akiranpk99 <nguyenhoangquangp@gmail.com>
2021-04-01 09:52:12 +0700

    init index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    require_once "./header.php"
+    ?>
+    <h1>Quản lý các sản phẩm</h1>
+    <a href="./Controllers/SanPhamController.php?action=DanhSach">Xem danh sách</a>
+    <a
+    href="./Views/AddSP.php">
+        Them Xem Danh Sach
+    </a>
+<?php 
+    require_once "./footer.php"
+    ?>
